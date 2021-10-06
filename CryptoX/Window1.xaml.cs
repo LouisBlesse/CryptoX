a83aafac2851c92c986a19bf22914bb70210e25a
LouisBlesse <63815688+LouisBlesse@users.noreply.github.com>
2021-10-06 15:30:20 +0200

    2nd page

--- CryptoX/Window1.xaml.cs
+++ CryptoX/Window1.xaml.cs
@@ -10,27 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-
 
 namespace CryptoX
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logique d'interaction pour Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Window1 p = new Window1();
-            p.Show();
-
         }
     }
 }
