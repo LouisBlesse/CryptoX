3a58382e88afc24b10ab93043b1f9a6e701fcdc6
LouisBlesse <louis.blesse@yahoo.fr>
2021-10-11 18:13:35 +0200

    don't work

--- CryptoX/LunarCrush.cs
+++ CryptoX/LunarCrush.cs
@@ -11,15 +11,42 @@ namespace CryptoX
 {
     class LunarCrush
     {
-        static HttpClient client = new HttpClient();
-        public static async Task<string>  Connect()
+        public HttpClient client { get; set; }
+
+        public LunarCrush()
         {
-            string res =
-                await client.GetStringAsync(
-                    "https://api.lunarcrush.com/v2?data=coinoftheday_info&key=0yhms1bivd391vg0g7klyb1");
-            return res;
+            client = new HttpClient();
         }
 
+        public async Task<string> Connect()
+        {
+
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader(await client.GetStringAsync(
+                       "https://api.lunarcrush.com/v2?data=coinoftheday_info&key=0yhms1bivd391vg0g7klyb1")))
+                {
+                    string line;
+                    // Read and display lines from the file until the end of
+                    // the file is reached.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        return line;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+
+            //string res =
+            //        await client.GetStringAsync(
+            //            "https://api.lunarcrush.com/v2?data=coinoftheday_info&key=0yhms1bivd391vg0g7klyb1");
+            //return res;
+        }
     }      
 }   
 
