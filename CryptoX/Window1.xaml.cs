d9aacdc8ee03259c42a4d6de1a974fc3da241bb1
LouisBlesse <63815688+LouisBlesse@users.noreply.github.com>
2021-10-14 22:14:07 +0200

    CheckBox wip

--- CryptoX/Window1.xaml.cs
+++ CryptoX/Window1.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Linq;
+using System.Threading;
 
 namespace CryptoX
 {
@@ -23,28 +25,48 @@ namespace CryptoX
         {
             InitializeComponent();
             Initialize();
+            //GetCheckBox(AllData);
         }
 
-        public async Task Initialize()
+        private async Task<LunarCrush.Root> Initialize()
         {
             LunarCrush Lune = new LunarCrush();
             val.Text = await Lune.Connect();
             List<string> AllName = new List<string>() { } ;
+            LunarCrush.Root tmp = Lune.transfert(val.Text);
+
 
             try
             {
-                LunarCrush.Root tmp = Lune.transfert(val.Text);
                 for (int i=0; i<tmp.data.Count; i++)
                 {
                     AllName.Add(tmp.data[i].name);
                 }
                 choix.ItemsSource = AllName;
+                
             }
             catch (Exception e)
             {
                 val2.Text = e.ToString();
             }
+            GetCheckBox(tmp);
+            return tmp;
+        }
 
+        private void GetCheckBox(LunarCrush.Root AllData)
+        {
+            val2.Text = "try"; //work
+            /*LunarCrush Lune = new LunarCrush();
+            var AllData = Lune.Connect();
+            var requete = from data in AllData.
+                          select name where ;*/
+            for (int i = 0; i < AllData.data.Count; i++)
+            {
+                if (AllData.data[i].name == choix.SelectedIndex.ToString())//dosn't work
+                {
+                    val2.Text = "La crypto monaie est le " + AllData.data[i].name + "sa valeure est de " + AllData.data[i].price + ".";
+                }
+            }
         }
     }
 }
